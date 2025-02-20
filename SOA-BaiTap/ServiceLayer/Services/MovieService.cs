8bba6fbe16129e1c591c38711886be32e62bfe36
Jiro <huymachgia555@gmail.com>
2025-02-20 15:32:39 +0700

    Change Branch First commit

--- SOA-BaiTap/ServiceLayer/Services/MovieService.cs
+++ SOA-BaiTap/ServiceLayer/Services/MovieService.cs
@@ -1,4 +1,5 @@
-﻿using SOA_BaiTap.CoreLayer.Entities;
+﻿using Humanizer;
+using SOA_BaiTap.CoreLayer.Entities;
 using SOA_BaiTap.RepositoryLayer.Interfaces;
 
 namespace SOA_BaiTap.ServiceLayer.Services
@@ -11,19 +12,29 @@ namespace SOA_BaiTap.ServiceLayer.Services
             _movieRepository = movieRepository;
         }
         //Crud Methods
-        public async Task<IEnumerable<Movie>> GetAllMoviesAsync()
-        {
-            return await _movieRepository.GetAllMoviesAsync();
-        }
         public async Task AddMovieAsync(Movie movie)
         {
+            var existingMovies = await _movieRepository.GetAllMoviesAsync();
+            if (existingMovies.Any(m => m.Title == movie.Title))
+            {
+                throw new ArgumentException("A movie with the same title already exists.");
+            }
             await _movieRepository.AddMovieAsync(movie);
         }
+
         // Stored Procedure Method
         public async Task<IEnumerable<Movie>> GetTopRatedMoviesWithSpAsync(int topCount)
         {
-            return await
-           _movieRepository.GetTopRatedMoviesWithSpAsync(topCount);
+            try
+            {
+                return await
+               _movieRepository.GetTopRatedMoviesWithSpAsync(topCount);
+            }
+            catch (Exception ex)
+            {
+                // Log error, provide a generic message, or rethrow
+                throw new ApplicationException("An error occurred while retrieving top - rated movies.", ex);
+            }
         }
 
     }
