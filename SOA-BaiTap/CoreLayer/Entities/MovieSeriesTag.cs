15a2873604dd918e50457f73697c773e057bd913
Jiro <huymachgia555@gmail.com>
2025-02-20 14:59:30 +0700

    Init Data AppDbContext

---
+++ SOA-BaiTap/CoreLayer/Entities/MovieSeriesTag.cs
@@ -0,0 +1,10 @@
+﻿namespace SOA_BaiTap.CoreLayer.Entities
+{
+    public class MovieSeriesTag
+    {
+        public int MovieSeriesId { get; set; }
+        public int TagId { get; set; }
+        public Movie Movie { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
---
+++ SOA-BaiTap/CoreLayer/Entities/Ratings.cs
@@ -0,0 +1,11 @@
+﻿namespace SOA_BaiTap.CoreLayer.Entities
+{
+    public class Ratings
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int MovieSeriesId {  get; set; }
+        public Movie MoviesSeries { get; set; }
+    }
+}
