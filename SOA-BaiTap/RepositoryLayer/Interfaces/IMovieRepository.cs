35e2045e767f5dfc75efdd2a5633403cb62f58d9
Jiro <huymachgia555@gmail.com>
2025-02-20 15:12:15 +0700

    Init Repository

---
+++ SOA-BaiTap/RepositoryLayer/Interfaces/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using SOA_BaiTap.CoreLayer.Entities;
+
+namespace SOA_BaiTap.RepositoryLayer.Interfaces
+{
+    public interface IMovieRepository
+    {
+        public Task<IEnumerable<Movie>> GetMoviesAsync();
+        public Task AddMovieAsync(Movie movie);
+        public Task UpdateMovieAsync(Movie movie);
+        public Task DeleteMovieAsync(int movieId);
+    }
+}
